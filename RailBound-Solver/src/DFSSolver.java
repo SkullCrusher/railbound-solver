c92171edfb60565b9c06d6f5c410f99d883ffb56
unknown <boxhead.two.play@gmail.com>
2022-10-04 19:19:38 -0700

    Added multi cart dfs

--- RailBound-Solver/src/DFSSolver.java
+++ RailBound-Solver/src/DFSSolver.java
@@ -7,6 +7,10 @@ import java.util.HashMap;
 
 public class DFSSolver implements Solver {
 
+    HashMap<Integer, Point> sLoc = new HashMap<>();
+    HashMap<Integer, Integer> sDir = new HashMap<>();
+
+
     public boolean loadConfiguration(String path) throws IOException {
         return Sim.loadFile(path);
     }
@@ -19,22 +23,30 @@ public class DFSSolver implements Solver {
         System.out.println("====\n");
 
         // Get the starting locations of each cart to prevent doing lookups.
-        HashMap<Integer, Point> sLoc = this.Sim.getCartStartingPos();
-        HashMap<Integer, Integer> sDir = this.Sim.getCartStartingDirection();
+        this.sLoc = this.Sim.getCartStartingPos();
+        this.sDir = this.Sim.getCartStartingDirection();
 
         // How many tiles are there.
         int tileCount = new Tile(new Point(0, 0), 0, false).getMovementMapCount();
 
         // Generate the possible solutions.
-        int totalSolutions = dfs(sLoc.get(1), sDir.get(1), this.Sim.getAvailableTrack(), tileCount, 0);
+        int totalSolutions = dfs(1, this.sLoc.get(1), this.sDir.get(1), this.Sim.getAvailableTrack(), tileCount, 0);
 
         System.out.println("solutions");
         System.out.println(totalSolutions);
     }
 
-    int dfs(Point pos, int direction, int trackPiecesLeft, int typeOfTrackPieces, int depth) throws IOException {
+    int dfs(int cart, Point pos, int direction, int trackPiecesLeft, int typeOfTrackPieces, int depth) throws IOException {
 
         // this.Sim.printMap();
+        if(this.sLoc == null || this.sDir == null){
+            return 0;
+        }
+
+        // Stop going down once we get to the last cart.
+        if(cart > this.Sim.getNumberOfCarts()){
+            return 0;
+        }
 
         // If the depth is over 1000, it's invalid.
         if(depth > 1000){
@@ -78,13 +90,20 @@ public class DFSSolver implements Solver {
             Point newPos = new Point(pos.x + newMove.x, pos.y + newMove.y);
 
             // Do the next item on the search from the move.
-            return dfs(newPos, newMove.direction, trackPiecesLeft, typeOfTrackPieces, depth + 1);
+            return dfs(cart, newPos, newMove.direction, trackPiecesLeft, typeOfTrackPieces, depth + 1);
         }
 
         int solutions = 0;
 
+        // Do the base simulation before the tile is added.
+        if(sLoc.get(cart + 1) != null) {
+            solutions += dfs(cart + 1, sLoc.get(cart + 1), sDir.get(cart + 1), trackPiecesLeft, typeOfTrackPieces, depth);
+        }
+
         // Try adding each piece to the map at the current location and following it.
         for(int i = 1; i <= typeOfTrackPieces; i += 1) {
+
+            // Simulate the child
 
             Tile newTile = new Tile(pos, i, false);
 
@@ -96,6 +115,12 @@ public class DFSSolver implements Solver {
             // Place each piece of track and attempt to follow it.
             this.Sim.setTileAtPos(pos, newTile);
 
+            // Do the simulations for the child carts (if possible).
+            if(sLoc.get(cart + 1) != null) {
+                solutions += dfs(cart + 1, this.sLoc.get(cart + 1), this.sDir.get(cart + 1), trackPiecesLeft - 1, typeOfTrackPieces, depth);
+            }
+
+            // Move the current cart.
             MovementItem newMove = newTile.CalcNextPosition(direction);
 
             // If there is a possible move.
@@ -103,7 +128,7 @@ public class DFSSolver implements Solver {
                 Point newPos = new Point(pos.x + newMove.x, pos.y + newMove.y);
 
                 // Do the depth first search base on the new location.
-                solutions += dfs(newPos, newMove.direction, trackPiecesLeft - 1, typeOfTrackPieces, depth);
+                solutions += dfs(cart, newPos, newMove.direction, trackPiecesLeft - 1, typeOfTrackPieces, depth);
             }
 
             // Remove it.
